cb3c6a9e3a30ab746858553b680e90ba5f465464
Bees <Bees@Bassem-Barakes-MacBook-Pro.local>
2014-09-17 16:17:22 +0100

    add user content

--- djeebaapp/app/config/database.php
+++ djeebaapp/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'djeebadb',
+			'username'  => 'root',
+			'password'  => 'djeebadb',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
--- djeebaapp/app/database/migrations/2014_09_17_125812_create_users_table.php
+++ djeebaapp/app/database/migrations/2014_09_17_125812_create_users_table.php
@@ -12,7 +12,23 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('users', function($t) {
+		 	            $t->bigIncrements('id');
+						$t->string('registered-id')->unique();
+						$t->string('editable-email');
+						$t->string('firstname');
+						$t->string('lastname');
+						$t->string('date-of-birth');
+						$t->string('gender');
+						$t->string('phone');
+						$t->string('img-file-name');
+						$t->string('account-type');
+						$t->boolean('email-confirmed');
+						$t->boolean('phone-confirmed');
+						$t->string('token');
+						$t->string('password', 64);
+		                $t->timestamps();
+		 });
 	}
 
 	/**
